f0c69e3e1887510b4a806fbd175a36f9f6671a79
ZayLinAung <zay198804@gmail.com>
2024-03-30 14:38:48 -0400

    Add translate.py

---
+++ backend/translate.py
@@ -0,0 +1,30 @@
+import os
+from google.cloud import translate_v2 as translate
+
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "API_KEY.json"
+
+
+def translate_text(target: str, text: str) -> dict:
+    """Translates text into the target language.
+
+    Target must be an ISO 639-1 language code.
+    See https://g.co/cloud/translate/v2/translate-reference#supported_languages
+    """
+
+    translate_client = translate.Client()
+
+    if isinstance(text, bytes):
+        text = text.decode("utf-8")
+
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    result = translate_client.translate(text, target_language=target)
+
+    print("Text: {}".format(result["input"]))
+    print("Translation: {}".format(result["translatedText"]))
+    print("Detected source language: {}".format(result["detectedSourceLanguage"]))
+
+    return result
+
+print(translate_text('en', 'နေ‌‌ကောင်းလား'))
